e45bc3fa470a688946d806600b67319daf480c45
gargour <amr.gara@sesame.com.tn>
2026-08-07 01:45:19 +0100

    Security: restrict CORS to frontend origins only

--- app/main.py
+++ app/main.py
@@ -18,9 +18,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8501", "http://frontend:8501"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "DELETE"],
     allow_headers=["*"],
 )
 
